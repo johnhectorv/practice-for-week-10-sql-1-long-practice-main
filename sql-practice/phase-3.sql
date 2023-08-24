9f7400198df3fe9a269366d7c3e85d89fc5d5e18
Tim Mairose <tmairose@gmail.com>
2023-08-23 22:10:16 -0600

    Phase 1,2, Part of 3 done

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,37 @@
 -- Your code here
+INSERT INTO customers (name, phone) VALUES ('Rachel', 1111111111);
+
+SELECT name, points FROM customers WHERE name = 'Rachel';
+
+UPDATE customers SET points = points+1 WHERE name = 'Rachel';
+
+INSERT INTO coffee_orders DEFAULT VALUES;
+
+INSERT INTO customers (name, email, phone) VALUES 
+('Monica', 'monica@friends.show', 2222222222),
+('Phoebe', 'phoebe@friends.show', 3333333333);
+
+UPDATE customers SET points = points+3 WHERE name = 'Phoebe';
+
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+
+UPDATE customers SET points = points+4 WHERE name = 'Rachel';
+UPDATE customers SET points = points+4 WHERE name = 'Monica';
+
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+INSERT INTO coffee_orders DEFAULT VALUES;
+
+SELECT name, points FROM customers WHERE name = 'Monica';
+SELECT name, points FROM customers WHERE name = 'Phoebe';
+SELECT name, points FROM customers WHERE name = 'Rachel';
+
+UPDATE customers SET points = 0 WHERE name = 'Rachel';
+
